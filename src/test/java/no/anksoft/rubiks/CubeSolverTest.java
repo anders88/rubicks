e07c6cba0ea4c7444ef1df3b77ae623490f17cb7
Anders Karlsen <karlsenanders@hotmail.com>
2011-06-11 23:00:59 +0200

    Added new solve test

--- src/test/java/no/anksoft/rubiks/CubeSolverTest.java
+++ src/test/java/no/anksoft/rubiks/CubeSolverTest.java
@@ -10,5 +10,25 @@ public class CubeSolverTest {
 		CubeSolver start = new CubeSolver(Cube.finished().turnFrontClock());
 		assertThat(start.solve()).containsExactly(Moves.FRONT_ANTI);
 	}
+
+	@Test
+	public void shouldHandleTwoMoves() throws Exception {
+		CubeSolver start = new CubeSolver(Cube.finished().turnBackClock().turnRightAnti());
+		assertThat(start.solve()).containsExactly(Moves.RIGHT_CLOCK,Moves.BACK_ANTI);
+	}
 	
+	@Test
+	public void shouldSolveFiveRandomMoves() throws Exception {
+		Cube cube = Cube.finished();
+		MoveGenerator moveGenerator = new MoveGenerator(cube);
+		moveGenerator.doRandomMove();
+		moveGenerator.doRandomMove();
+		moveGenerator.doRandomMove();
+		moveGenerator.doRandomMove();
+		moveGenerator.doRandomMove();
+		CubeSolver start = new CubeSolver(cube);
+		assertThat(start.solve()).hasSize(3);
+		
+	}
+
 }
